645f8773b73ece9c37375b785f0674858d4188e2
Oscar Yanez-Suarez <oscar.yasu@gmail.com>
2021-06-15 20:17:09 -0500

    Fix sys.argv indexing

--- src/pyRehaStimPacketTest.py
+++ src/pyRehaStimPacketTest.py
@@ -1,16 +1,16 @@
 import sys
 
-import pyRehaStimNode
-import pyRehaStimPacket
+import pyRehaStimNode as rhnode
+import pyRehaStimPacket as rhpacket
 
 if len(sys.argv) != 3:
     print(f'{sys.argv[0]} [send|receive] device')
 elif not sys.argv[1] in {'send', 'receive'}:
     print(f'{sys.argv[0]} [send|receive] device')
 else:
-    node = pyRehaStimNode(sys.argv[1])
-    packet = pyRehaStimInitPacket(node.packet_count)
+    node = rhnode.pyRehaStimNode(sys.argv[2])
+    packet = rhpacket.pyRehaStimInitPacket(node.packet_count)
     if sys.argv[1] == 'send':
         node.send(packet.packet)
     else:
-        pyRehaStimPacket.parse_packet(node.receive())
+        rhpacket.pyRehaStimPacket.parse_packet(node.receive())
