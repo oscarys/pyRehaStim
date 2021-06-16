c482c36f8f220494ca2b4386d3b35692053d498e
Oscar Yanez-Suarez <oscar.yasu@gmail.com>
2021-06-15 22:30:43 -0500

    Echo packet number in INITACK

--- src/pyRehaStimPacketTest.py
+++ src/pyRehaStimPacketTest.py
@@ -26,6 +26,6 @@ else:
             if packet_bytes:
                 pid, cmd = rhpacket.pyRehaStimPacket.parse_packet(packet_bytes)
                 if cmd == rhpacket.pyRehaStimPacket.INIT:
-                    print(f'Sending INITACK {node.packet_count}')
-                    node.send_packet(rhpacket.pyRehaStimInitAckPacket(node.packet_count))
+                    print(f'Sending INITACK {pid}')
+                    node.send_packet(rhpacket.pyRehaStimInitAckPacket(pid))
                     break
