a795ed5f87e6df6795d017c30c842ca3054b56c6
oscarys <oscar.yasu@gmail.com>
2022-07-01 03:32:13 -0500

    Refactored rsPacket into RSPACKET

--- src/rsPacket.py
+++ src/rsPacket.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-Oscar Yáñez Suárez, 2021.06.16
+Oscar Yáñez Suárez, 2022.07.01
 
 This work is licensed under the Creative Commons Attribution 4.0
 International License. To view a copy of this license, visit
@@ -14,7 +14,7 @@ import crccheck.checksum
 
 # Packet class
 
-class rsPacket:
+class RSPACKET:
 
     # Packet types
 
@@ -41,87 +41,100 @@ class rsPacket:
     ### Class methods
 
     # Get packet type from command int
-    def packet_type_from_cmd(cmd):
-        for type in rsPacket.TYPES:
-            if rsPacket.TYPES[type] == cmd:
+    def cmd_to_type(cmd):
+        for type in RSPACKET.TYPES:
+            if RSPACKET.TYPES[type] == cmd:
                 return type
 
     # "byte stuffing", i.e, xoring with STUFFING_KEY
     def stuff_byte(byte):
-        return ((byte & ~rsPacket.STUFFING_KEY) | (~byte & rsPacket.STUFFING_KEY))
-
-    # Build packet from byte string
-    def parse_packet(packet_id, bytes):
-        # Convert byte string to byte sub-lists
-        packet_lead = [byte for byte in bytes[0:5]]
-        packet_payload = [byte for byte in bytes[5:-1]]
-        packet_trail = [bytes[-1]]
-        # Compute stuffed payload checksum and length
-        checksum = rsPacket.stuff_byte(crccheck.crc.Crc8.calc(packet_payload))
-        datalength = rsPacket.stuff_byte(len(packet_payload))
-        # Verify packet integrity
-        if packet_lead[0] == rsPacket.START_BYTE and packet_trail[0] == rsPacket.STOP_BYTE:
-            if packet_lead[1] == rsPacket.STUFFING_BYTE and packet_lead[3] == rsPacket.STUFFING_BYTE:
-                if checksum == packet_lead[2]:
-                    if datalength == packet_lead[4]:
-                        return rsPacket(packet_payload[0], rsPacket.packet_type_from_cmd(packet_payload[1]), packet_payload[2:])
-                    else:
-                        raise ValueError('Wrong payload length')
-                else:
-                    raise ValueError('Wrong payload checksum')
-            else:
-                raise ValueError('Bad stuffing byte in packet')
-        else:
-            raise ValueError('Unknown start/stop byte in packet')
+        return ((byte & ~RSPACKET.STUFFING_KEY) | (~byte & RSPACKET.STUFFING_KEY))
 
     ### Instance methods
 
     # Packet constructor from stream id, type string and data list
-    def __init__(self, packet_id, packet_type, packet_data):
-        # Packet identification number in the communication stream
-        self.packet_id = packet_id
-        # Packet type and command
-        self.packet_type = packet_type
-        self.packet_command = self.TYPES[packet_type]
-        # Packet payload array
-        self.__packet_payload = [packet_id, self.packet_command] + packet_data
+    def __init__(self, *, packet=None, raw_packet=None):
+        # Build packet from bytearray
+        if raw_packet != None and packet == None:
+            # Convert byte string to byte sub-lists
+            packet_lead = [byte for byte in raw_packet[0:5]]
+            packet_payload = [byte for byte in raw_packet[5:-1]]
+            packet_trail = [raw_packet[-1]]
+            # Compute stuffed payload checksum and length
+            checksum = RSPACKET.stuff_byte(crccheck.crc.Crc8.calc(packet_payload))
+            datalength = RSPACKET.stuff_byte(len(packet_payload))
+            # Verify packet integrity
+            if packet_lead[0] == RSPACKET.START_BYTE and packet_trail[0] == RSPACKET.STOP_BYTE:
+                if packet_lead[1] == RSPACKET.STUFFING_BYTE and packet_lead[3] == RSPACKET.STUFFING_BYTE:
+                    if checksum == packet_lead[2]:
+                        if datalength == packet_lead[4]:
+                            # Build packet object
+                            # Packet identification number in the communication stream
+                            self._packet_id = packet_payload[0]
+                            # Packet type and command
+                            self._packet_type = RSPACKET.cmd_to_type(packet_payload[1])
+                            self._packet_command = packet_payload[1]
+                            # Packet payload array
+                            self._packet_payload = [self._packet_id, self._packet_command] + packet_payload[2:]
+                            #return rsPacket(packet=(packet_payload[0], rsPacket.packet_type_from_cmd(packet_payload[1]), packet_payload[2:]))
+                        else:
+                            raise ValueError('Wrong payload length')
+                    else:
+                        raise ValueError('Wrong payload checksum')
+                else:
+                    raise ValueError('Bad stuffing byte in packet')
+            else:
+                raise ValueError('Unknown start/stop byte in packet')
+        # Build packet from packet info
+        elif raw_packet == None and packet != None:
+            p_id, p_type, p_data = packet
+            # Packet identification number in the communication stream
+            self._packet_id = p_id
+            # Packet type and command
+            self._packet_type = p_type
+            self._packet_command = RSPACKET.TYPES[self._packet_type]
+            # Packet payload array
+            self._packet_payload = [self._packet_id, self._packet_command] + p_data
+        # Signal error in the call
+        else:
+            raise ValueError('Either packet info or raw bytes must be provided, but not both')
         # Compute (stuffed) payload checksum
-        self.__checksum = rsPacket.stuff_byte(crccheck.crc.Crc8.calc(self.__packet_payload))
+        self._checksum = RSPACKET.stuff_byte(crccheck.crc.Crc8.calc(self._packet_payload))
         # Compute (stuffed) payload length
-        self.__datalength = rsPacket.stuff_byte(len(self.__packet_payload))
-        # Build other packet components
-        self.__packet_lead = [self.START_BYTE, self.STUFFING_BYTE, self.__checksum, self.STUFFING_BYTE, self.__datalength]
-        self.__packet_trail = [self.STOP_BYTE]
+        self._datalength = RSPACKET.stuff_byte(len(self._packet_payload))
+        # Build packet as a list
+        self._packet = [self.START_BYTE, self.STUFFING_BYTE, self._checksum, self.STUFFING_BYTE, self._datalength]
+        self._packet = self._packet + self._packet_payload + [self.STOP_BYTE]
+        # Build packet as a bytearray
+        self._raw_packet = b''.join([byte.to_bytes(1, 'little') for byte in self._packet])
+
 
     # Packet print as text
     def __str__(self):
         packet_dec = self.get_packet_as_dec()
         packet_hex = self.get_packet_as_hex()
-        return f'[{self.packet_id:03d}] {self.packet_type:23s}: {packet_dec} \t {packet_hex}'
+        return f'[{self._packet_id:03d}] {self._packet_type:23s}: {packet_dec} \t {packet_hex}'
 
-    # Build and return packet as a byte list
-    def get_packet_as_list(self):
-        return self.__packet_lead + self.__packet_payload + self.__packet_trail
-
-    # Build and return packet as a decimal string
+    # Encode packet as a decimal string
     def get_packet_as_dec(self):
-        return '|'.join([f'{b:03d}' for b in self.get_packet_as_list()])
+        return '|'.join([f'{b:03d}' for b in self._packet])
     
-    # Build and return packet as a hex string
+    # Encode packet as a hex string
     def get_packet_as_hex(self):
-        return '|'.join(['{:>02s}'.format(hex(b)[2:]) for b in self.get_packet_as_list()]).upper()
+        return '|'.join(['{:>02s}'.format(hex(b)[2:]) for b in self._packet]).upper()
 
     # Build and return packet as a byte string
-    def get_packet_as_bytes(self):
-        return b''.join([byte.to_bytes(1, 'little') for byte in self.get_packet_as_list()])
+    def get_packet(self):
+        return self._raw_packet
 
 
 # Module testing
 if __name__ == '__main__':
-    for i, type in enumerate(rsPacket.TYPES):
+    for i, type in enumerate(RSPACKET.TYPES):
         # Create each packet with arbitrary payload data
-        packet = rsPacket(i, type, [100 + i, 101 + i])
-        print(packet)
+        p = RSPACKET(packet=(i, type, [100 + i, 101 + i]))
+        print(f'Encode: {p}')
         # Re-parse packet and display for comparison
-        parsed_packet = rsPacket.parse_packet(i, packet.get_packet_as_bytes())
-        print(parsed_packet)
+        pp = RSPACKET(raw_packet=p.get_packet())
+        print(f'Decode: {pp}')
+        print('-'*50)
